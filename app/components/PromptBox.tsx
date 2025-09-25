2c141b7b8d039d17b58b9b060f736ff373dcaee0
Doan Van An <andv@vcsvietnam.vn>
2025-09-25 16:19:56 +0700

    first commit

---
+++ app/components/PromptBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PromptBox = () => {
+  return <form className=""></form>;
+};
+
+export default PromptBox;
---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-Inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DeepSeek - Andv",
+  description: "AI Full Stack Project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
